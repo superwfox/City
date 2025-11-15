1137eef217d259e3f25e9b35277605f2db83a491
SUDARK <2963502563@qq.com>
2025-11-15 21:35:35 +0800

    Sudark

---
+++ src/main/java/sudark2/Sudark/city/City.java
@@ -0,0 +1,17 @@
+package sudark2.Sudark.city;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class City extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/sudark2/Sudark/city/command/CityCommand.java
@@ -0,0 +1,17 @@
+package sudark2.Sudark.city;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+
+public class CityCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
+        if()
+
+        return false;
+    }
+
+
+}
